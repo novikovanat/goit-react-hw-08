52c9e758353ded86476aacd2f9175a97b2cac0ad
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-23 20:22:22 +0200

    condition added

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,16 @@
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/authSelectors";
 
 export default function AppBar() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  console.log(isLoggedIn);
   return (
     <div>
       <Navigation />
-      <UserMenu />
-      <AuthNav />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </div>
   );
 }
