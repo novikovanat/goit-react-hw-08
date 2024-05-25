89ebb2ad2ddae958f44eb05e651b70c7f5ab5860
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-25 17:37:49 +0200

    reset form added

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -13,14 +13,9 @@ export default function LoginForm() {
   const dispatch = useDispatch();
   const handleSubmit = (values, actions) => {
     dispatch(login(values));
-    console.log(values);
-    // actions.resetForm();
+    actions.resetForm();
   };
   const validationSchema = Yup.object().shape({
-    // name: Yup.string()
-    //   .min(3, "Username should be longer than two signs")
-    //   .max(18, "Username should be shorter  than nineteen  signs")
-    //   .required("Please enter your username"),
     email: Yup.string().email().required("Please enter your email"),
     password: Yup.string()
       .min(8, "Password should be longer than seven signs")
@@ -29,47 +24,37 @@ export default function LoginForm() {
   });
   return (
     <div>
-    <Formik
-      initialValues={{
-        // name: "",
-        email: "",
-        password: "",
-      }}
-      onSubmit={handleSubmit}
-      validationSchema={validationSchema}
-    >
-      <Form className={css.form}>
-        {/* <div className={css.flex}>
-          <label htmlFor={userNameFieldId}>Username</label>
-          <Field id={userNameFieldId} name="name" />
-          <ErrorMessage className={css.color} name="name" component="span" />
-        </div> */}
-        <div className={css.flex}>
-          <label htmlFor={emailFieldId}>Email</label>
-          <Field id={emailFieldId} name="email" type="email" />
-          <ErrorMessage className={css.color} name="email" component="span" />
-        </div>
-        <div className={css.flex}>
-          <label htmlFor={passwordFieldId}>Password</label>
-          <Field
-            id={passwordFieldId}
-            name="password"
-            type="password"
-            autoComplete="new-password"
-          />
-          <ErrorMessage
-            className={css.color}
-            name="password"
-            component="span"
-          />
-        </div>
-        <Button
-          buttonName="login"
-          buttonType="submit"
-          buttonValue="Log in"
-        />
-      </Form>
-    </Formik>
-  </div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className={css.form}>
+          <div className={css.flex}>
+            <label htmlFor={emailFieldId}>Email</label>
+            <Field id={emailFieldId} name="email" type="email" />
+            <ErrorMessage className={css.color} name="email" component="span" />
+          </div>
+          <div className={css.flex}>
+            <label htmlFor={passwordFieldId}>Password</label>
+            <Field
+              id={passwordFieldId}
+              name="password"
+              type="password"
+              autoComplete="new-password"
+            />
+            <ErrorMessage
+              className={css.color}
+              name="password"
+              component="span"
+            />
+          </div>
+          <Button buttonName="login" buttonType="submit" buttonValue="Log in" />
+        </Form>
+      </Formik>
+    </div>
   );
 }
